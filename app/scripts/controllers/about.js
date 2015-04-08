dce9ebcfea609f8fb293c2a16cfd08e955423e07
sagar singh <ssing128@gmail.com>
2015-04-08 01:06:45 -0700

    took out unnecessary code from yeoman generator

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -9,9 +9,4 @@
  */
 angular.module('godviewApp')
   .controller('AboutCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
   });
